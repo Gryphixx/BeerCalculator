a9326638a2ce680d7c67ad33762c14a222dd46c3
Lander Maerievoet <landermaerievoet@gmail.com>
2018-03-16 15:44:09 +0100

    lelbj

--- BeerCalculator/BeerCalculator/BeerCalculator/BeerType.cs
+++ BeerCalculator/BeerCalculator/BeerCalculator/BeerType.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace BeerCalculator
 {
     class BeerType
     {
+        public string BeerName { get; }
+        public List<BeerSize> Sizes = new List<BeerSize>();
+
+        public void FillPickerWithSizes(Picker pckr)
+        {
+            foreach (BeerSize size in Sizes)
+            {
+
+            }
+        }
+        
+        public BeerType(string beerName)
+        {
+            this.BeerName = BeerName;
+        }
     }
 }
