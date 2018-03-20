8c4d2ffbb2c84d40bf9152b2512ccefbbf31e5cb
Lander Maerievoet <landermaerievoet@gmail.com>
2018-03-20 09:54:55 +0100

    hey

--- BeerCalculator/BeerCalculator/BeerCalculator/Persistence.cs
+++ BeerCalculator/BeerCalculator/BeerCalculator/Persistence.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using MySql.Data;
 
 namespace BeerCalculator
 {
     class Persistence
     {
+        public List<BeerType> GetBeerList()
+        {
 
+        }
     }
 }
