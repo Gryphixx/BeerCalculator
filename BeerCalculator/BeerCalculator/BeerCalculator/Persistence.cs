83ef2e888e08dd8b4db18670d8f5ec7726ceda6c
Lander Maerievoet <landermaerievoet@gmail.com>
2018-03-20 10:30:42 +0100

    hih

--- BeerCalculator/BeerCalculator/BeerCalculator/Persistence.cs
+++ BeerCalculator/BeerCalculator/BeerCalculator/Persistence.cs
@@ -4,15 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
-using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace BeerCalculator
 {
     class Persistence
     {
+        static string conString = "server=maerievoet.ddns.net;user id=GIP;port=3306;database=beercalculator; password=Usman123";
+        MySqlConnection con = new MySqlConnection(conString);
+
         public List<BeerType> GetBeerList()
         {
-
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM beercalculator.bieren;", con);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable(strDataTableName);
+            da.Fill(dt);
+            return dt;
         }
     }
 }
